437eb67fee87c9614391d1cbc33d02ac412d9c4d
Michael Bishop <mbtyke@yahoo.com>
2014-03-09 15:22:32 -0400

    initial commit

---
+++ lib/pry/mulligan.rb
@@ -0,0 +1,7 @@
+require "pry/mulligan/version"
+
+module Pry
+  module Mulligan
+    # Your code goes here...
+  end
+end
---
+++ lib/pry/mulligan/version.rb
@@ -0,0 +1,5 @@
+module Pry
+  module Mulligan
+    VERSION = "0.0.1"
+  end
+end
